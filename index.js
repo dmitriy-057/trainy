b0837eb1246c9124dc825a7b1f363f87be5a2f2e
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-01-06 00:55:41 +0200

    add DOM

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
-  const allValues = [];
-  console.log(allValues);
+const listEl = document.querySelector('ul')
+console.log('listEl', listEl)
 
-  function getAllPropValues(propName) {
-    // Change code below this line
-
-    
-  for (const product of products) {
-    console.log(product);
-    
-    if(propName === product.name) {
-      allValues.push(product.name);
-    } 
-      else if(propName === product.quantity) {
-        allValues.push(product.quantity)
-    } 
-      else if(propName === product.price) {
-      allValues.push(product.price)
-    } 
-    return allValues;
-  }
-    // Change code above this line
-  }
-  
+const itemEl = listEl.querySelector('li');
+console.log('itemEl',itemEl);
+const textEl = document.querySelector('p');
+console.log('textEl',textEl);
+textEl.textContent = 'CAAAAT'
