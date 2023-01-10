cb2f654bd9e57b469d6246febd0cef406908f27a
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-01-11 00:18:31 +0200

    добавляет DOM

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
-const listEl = document.querySelector('ul')
-console.log('listEl', listEl)
+import products from './products.js';
 
-const itemEl = listEl.querySelector('li');
-console.log('itemEl',itemEl);
-const textEl = document.querySelector('p');
-console.log('textEl',textEl);
-textEl.textContent = 'CAAAAT'
+const makeProductsTableMarkUp = ({id,name,description, price}) => {
+return `<tr>
+<td>
+  ${id}
+</td>
+<td>
+  ${name}
+</td>
+<td>
+  ${description}
+</td>
+<td>
+  ${price}
+</td>
+</tr> `
+}
+
+const createProductsTable = products.map(makeProductsTableMarkUp).join('') ;
+console.log(createProductsTable)
+
+const tableEl = document.querySelector('.js-table');
+tableEl.insertAdjacentHTML('beforeend', createProductsTable);
+
+
+// const makeCard = ({name, description, price}) => {
+//     const articleEl = document.createElement('article');
+//     articleEl.classList.add('product');
+    
+//     const nameEl = document.createElement('h2');
+//     nameEl.classList.add('product__name');
+//     nameEl.textContent = name;
+    
+//     const descrEl = document.createElement('p');
+//     descrEl.classList.add('product__descr');
+//     descrEl.textContent = description;
+    
+//     const priceEl = document.createElement('p');
+//     priceEl.classList.add('product__price');
+//     priceEl.textContent = `Цена: ${price} $`;
+    
+    
+//     articleEl.append(nameEl, descrEl, priceEl);
+//     console.log('articleEl', articleEl);
+//     return articleEl
+// }
+
+
+
+
+// const elements = products.map(makeCard);
+// const containerEl = document.querySelector('.js-container');
+// containerEl.append(...elements);
---
+++ products.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        name: 'Телефон',
+        description: 'описание для телефона',
+        price: 2000,
+        available: true,
+        onSale: true,
+        id: 213,
+    },
+    {
+        name: 'Планшет',
+        description: 'описание для планшета',
+        price: 1000,
+        available: false,
+        onSale: true,
+        id: 304
+    },
+    {
+        name: 'Пылесос',
+        description: 'описание для пылесоса',
+        price: 3000,
+        available: false,
+        onSale: false,
+        id:54
+    },
+]
