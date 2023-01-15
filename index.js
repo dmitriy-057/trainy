726e42d60d63e63ad770617a9cf752a07fccb060
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-01-15 23:56:40 +0200

    add modal events

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const outPutEl = document.querySelector('.js-output');
-const clearBtn = document.querySelector('button');
-
-window.addEventListener('keypress', onKeyPress);
-clearBtn.addEventListener('click', onClearOutput)
-function onKeyPress(event) {
-  console.log(event.key);
-  outPutEl.textContent += event.key;
+const refs = {
+  openModalBtn: document.querySelector('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector('.js-backdrop'),
 }
 
-function onClearOutput() {
-  outPutEl.textContent = ''
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClick)
+
+function onOpenModal() {
+  window.addEventListener('keydown', onEscKeyPress);
+  document.body.classList.add('show-modal');
+};
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('show-modal')
+};
+
+function onBackdropClick(event) {
+  console.log('currentTarget',event.currentTarget);
+  console.log('target', event.target);
+
+  if(event.currentTarget === event.target) {
+    onCloseModal(); 
+  }
+};
+
+function onEscKeyPress(event) {
+  console.log(event);
+  if(event.code === 'Escape') {
+    onCloseModal(); 
+  }
 }
